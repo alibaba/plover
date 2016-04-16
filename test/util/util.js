22bd9b016e56248b13aeb1d4949acf52362bdd6a
bencode <bencode@163.com>
2016-04-16 18:06:18 +0800

    test(util): coverage

--- test/util/util.js
+++ test/util/util.js
@@ -15,18 +15,22 @@ describe('util/util', function() {
     const services = {
       $get: function(name) {
         return new Service(name);
-      }
+      },
+
+      myName: 'hello'
     };
 
-    gutil.delegateGetters(services, ['dsService', 'offerService']);
-    services.dsService.name.should.be.equal('dsService');
+    gutil.delegateGetters(services, ['dsService', 'offerService', 'myName']);
+    services.dsService.name.should.equal('dsService');
+    services.myName.should.equal('hello');
   });
 
 
   it('util.loadModule', function() {
     const root = pathUtil.join(__dirname, '../fixtures/util/util');
-    gutil.loadModule(root, './lib/a').should.be.equal('a');
-    gutil.loadModule(root, 'a').should.be.equal('a');
+    gutil.loadModule(root, './lib/a').should.equal('a');
+    gutil.loadModule(root, 'a').should.equal('a');
+    gutil.loadModule(root, pathUtil.join(root, 'lib/a')).should.equal('a');
   });
 });
 
