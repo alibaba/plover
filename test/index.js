0421cfa6928cf043d246b1fc68ba17ce4709acf7
bencode <bencode@163.com>
2016-04-19 13:04:58 +0800

    feat: 完成基础功能和组织形式
    1. 部件的组织结构
    2. 配置文件的加载
    3. 日志级别等参数的设置

---
+++ test/index.js
@@ -0,0 +1,127 @@
+'use strict';
+
+
+const pathUtil = require('path');
+const koa = require('koa');
+const request = require('supertest');
+
+const plover = require('..');
+const Logger = require('plover-logger');
+
+
+/* eslint no-process-env: 0 */
+
+
+describe('application', function() {
+  const root = pathUtil.join(__dirname, 'fixtures/index');
+  const configRoot = pathUtil.join(root, 'config');
+
+
+  it('可以使用plover()构造应用', function() {
+    const app = plover({ applicationRoot: root });
+    app.server.should.be.instanceof(require('koa/lib/application'));
+  });
+
+
+  it('可以通过app.settings取得应用配置配置', function() {
+    const settings = { applicationRoot: root };
+    const app = plover(settings);
+    app.settings.applicationRoot.should.equal(root);
+  });
+
+
+  it('可以指定configRoot来读取业务配置', function() {
+    const app = plover({
+      applicationRoot: root,
+      configRoot: configRoot
+    });
+
+    app.config.plover.should
+        .equal(require(pathUtil.join(configRoot, 'plover')));
+
+    app.config.urls.should
+        .equal(require(pathUtil.join(configRoot, 'urls')));
+  });
+
+
+  it('settings配置会覆盖config中相同的配置', function() {
+    const app = plover({
+      applicationRoot: root,
+      configRoot: configRoot,
+      urls: []
+    });
+
+    app.config.plover.should
+        .equal(require(pathUtil.join(configRoot, 'plover')));
+
+    app.config.urls.should.eql([]);
+  });
+
+
+  it('可以通过配置设置日志级别', function() {
+    plover({ applicationRoot: root, logger: { level: 'info' } });
+    Logger.level.should.equal('info');
+  });
+
+
+  it('可以使用外部koa app对象', function() {
+    const app = koa();
+    const papp = plover(app, {
+      applicationRoot: root
+    });
+
+    papp.server.should.be.equal(app);
+  });
+
+
+  it('可以通过app.context扩展koa对象', function() {
+    const p = plover({ applicationRoot: root });
+
+    p.context.sayHello = function() {
+      return 'hello';
+    };
+
+    const app = p.server;
+    app.use(function* () {
+      this.body = this.sayHello();
+    });
+
+    return request(app.callback()).get('/').expect('hello');
+  });
+
+
+  it('设置DEBUG环境变量会调整日志级别为debug', function() {
+    process.env.DEBUG = 'plover*';
+    plover({ applicationRoot: root });
+    Logger.level.should.equal('debug');
+    delete process.env.DEBUG;
+  });
+
+
+  it('可以通过LOG_LEVEL来设置日志级别', function() {
+    process.env.LOG_LEVEL = 'info';
+    plover({ applicationRoot: root });
+    Logger.level.should.equal('info');
+    delete process.env.LOG_LEVEL;
+  });
+
+
+  it('模块依赖如果不兼容会报错的', function(done) {
+    const path = pathUtil.join(__dirname, 'fixtures/index-dep');
+    const app = plover({ applicationRoot: path });
+
+    app.on('error', function(e) {
+      e.message.should.be.match(
+        'b@2.0.0 is not compatible with a which depend on b@~1.0.0');
+      done();
+    });
+  });
+
+
+  it('application启动后会输出模块列表日志(for coverage)', function(done) {
+    const path = pathUtil.join(__dirname, 'fixtures/index-mods');
+    plover({ applicationRoot: path, env: 'test' });  // 测试环境不打印
+    plover({ applicationRoot: path, __testPrintModules: true }); // 会打印出模块列表
+    setTimeout(done, 10);
+  });
+});
