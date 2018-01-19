14534dbf2d62fb303abc96cdc32b38a51eeb13fe
bencode <bencode@163.com>
2018-01-19 22:29:27 +0800

    test: test for routes with namespace

--- packages/plover-router/test/plugin.js
+++ packages/plover-router/test/plugin.js
@@ -1,10 +1,9 @@
 const mm = require('plover-test-mate');
-const co = require('co');
 
 
 describe('plover-route/lib/plugin', () => {
-  it('config routes', () => {
-    const app = mm({
+  it('config routes', async() => {
+    const app = create({
       applicationRoot: __dirname,
       routes: r => {
         r.get('/profile', 'users#show');
@@ -14,41 +13,60 @@ describe('plover-route/lib/plugin', () => {
       }
     });
 
-    app.install('plover-web');
-    app.install(require('../lib/plugin'));
-
     app.use(ctx => {
       if (ctx.route) {
         ctx.body = ctx.route;
       }
     });
 
-    return co(function* () {
-      yield app.get('/profile').expect({
-        module: 'users',
-        action: 'show',
-        query: {}
-      });
-
-      yield app.get('/hello').expect('hello');
+    await app.get('/profile').expect({
+      module: 'users',
+      action: 'show',
+      query: {}
     });
+
+    await app.get('/hello').expect('hello');
+  });
+
+
+  it('routes with namespace', async() => {
+    const app = create({
+      applicationRoot: __dirname,
+      disableDefaultRouter: true,
+      routes: r => {
+        r.namespace('/users', () => {
+          r.use(async(ctx, next) => {
+            await next();
+            if (ctx.route) {
+              ctx.body = ctx.route.url;
+            } else {
+              ctx.body = ctx.path;
+            }
+          });
+
+          r.get('config', 'users#config');
+        });
+      },
+    });
+
+    await app.get('/users/config').expect('users:config');
+    await app.get('/users/messages').expect('/users/messages');
+    await app.get('/users/').expect('/users/');
+    await app.get('/users').expect(404);
   });
 
 
   it('routes not config', () => {
-    const app = mm({
+    const app = create({
       applicationRoot: __dirname
     });
-
-    app.install('plover-web');
-    app.install(require('../lib/plugin'));
 
     (true).should.be.ok();
   });
 
 
   describe('http method', () => {
-    const app = mm({
+    const app = create({
       applicationRoot: __dirname,
       web: {
         csrf: {
@@ -57,29 +75,30 @@ describe('plover-route/lib/plugin', () => {
       }
     });
 
-    app.install('plover-web');
-    app.install(require('../lib/plugin'));
-
     app.use(ctx => {
       ctx.body = ctx.method;
     });
 
 
-    it('put with _method', () => {
-      return co(function* () {
-        yield app.agent.post('/update')
-          .send({ _method: 'put' })
-          .expect('PUT');
-      });
+    it('put with _method', async() => {
+      await app.agent.post('/update')
+        .send({ _method: 'put' })
+        .expect('PUT');
     });
 
 
-    it('patch with header: x-http-method-override', () => {
-      return co(function* () {
-        yield app.agent.post('/update')
-          .set('X-HTTP-Method-Override', 'patch')
-          .expect('PATCH');
-      });
+    it('patch with header: x-http-method-override', async() => {
+      await app.agent.post('/update')
+        .set('X-HTTP-Method-Override', 'patch')
+        .expect('PATCH');
     });
   });
 });
+
+
+function create(opts) {
+  const app = mm(opts);
+  app.install('plover-web');
+  app.install(require('../lib/plugin'));
+  return app;
+}
