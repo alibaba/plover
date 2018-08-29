07b7bf538ed28ba854aecbfbab1d70d96f6adc24
bencode <bencode@163.com>
2018-08-29 17:18:04 +0800

    feat: 优化异常的处理

--- packages/plover/lib/util/error-handler.js
+++ packages/plover/lib/util/error-handler.js
@@ -20,11 +20,13 @@ module.exports = function(config) {
       logger.error(e);
       ctx.app.emit('error', e, ctx);
       ctx.status = status;
+
       const message = development ?
         '<pre>' + util.inspect(e) + '\n' + (e.stack || '') + '</pre>' :
         'Internel Server Error';
 
-      if (ctx.is('json')) {
+      if (ctx.is('application/json') ||
+          !ctx.accepts('html') && ctx.accepts('application/json')) {
         ctx.body = { success: false, message };
       } else {
         ctx.body = message;
